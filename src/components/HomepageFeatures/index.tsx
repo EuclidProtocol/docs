2361f6e859da1fea8d2331b7df80afb5075c0c19
codewizard26 <nikhilmishra2608@gmail.com>
2024-05-30 21:08:05 +0530

    fix

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -12,16 +12,6 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: "Easy to Integrate",
-    Svg: require("@site/static/img/integrate_dark.svg").default,
-    description: (
-      <>
-        Euclid API was designed from the ground up to easily be integrated into
-        your application and get it running up quickly.
-      </>
-    ),
-  },
-  {
     title: "Modular",
     Svg: require("@site/static/img/modular.svg").default,
     description: (
@@ -38,6 +28,16 @@ const FeatureList: FeatureItem[] = [
       <>
         Euclid can be integrated instantly into smart contracts or an interface
         without requiring any payment or key.
+      </>
+    ),
+  },
+  {
+    title: "Easy to Integrate",
+    Svg: require("@site/static/img/integrate_dark.svg").default,
+    description: (
+      <>
+        Euclid API was designed from the ground up to easily be integrated into
+        your application and get it running up quickly.
       </>
     ),
   },
